bfb66f80bcbe876e5a51e2e505578ab20f706569
Ilya Ezhov <angryhat@mail.ru>
2019-03-28 02:15:16 +0300

    Task 8 done

---
+++ Ezhov_Ilya_Task08/Task1/DynamicArray.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class DynamicArray<T> where T : new()
+    {
+        private int _length;
+        public int Length
+        {
+            get
+            {
+                return _length;
+            }
+            set
+            {
+                _length = value;
+            }
+        }
+        private T[] _array;
+        public int Capacity
+        {
+            get
+            {
+                return _array.Length;
+            }
+        }
+
+        public DynamicArray()
+        {
+            _array = new T[8];
+            Length = 0;
+        }
+        public DynamicArray(int inputLength)
+        {
+            _array = new T[inputLength];
+            Length = 0;
+        }
+        public DynamicArray(T[] inputArray)
+        {
+            _array = inputArray;
+            Length = inputArray.Length;
+        }
+
+        public void Add(T input)
+        {
+            if (Length >= Capacity)
+            {
+                Array.Resize(ref _array, Capacity * 2);
+            }
+            _array[Length] = input;
+            Length++;
+        }
+        public void AddRange(T[] inputArray)
+        {
+            if (inputArray.Length + Length >= Capacity)
+            {
+                Array.Resize(ref _array, Capacity + inputArray.Length);
+            }
+            Array.Copy(inputArray, 0, _array, Length, inputArray.Length);
+            Length += inputArray.Length;
+        }
+        public void ShowArray()
+        {
+            Console.WriteLine($"Array capacity: {Capacity}. Array length: {Length}. Array elements:");
+            for (int i = 0; i < Length; i++)
+            {
+                Console.Write($"[{i}] = {_array[i],4}\t");
+                if (i!= 0 && (i % 5 == 0)) { Console.WriteLine(); }
+            }
+            Console.WriteLine("\n\n");
+        }
+        public bool Remove(int index)
+        {
+            if (_array[index] != null && index >= 0 && index < Length)
+            {
+                for (int i = index; i < Length - 1; i++)
+                {
+                    _array[i] = _array[i + 1];
+                }
+                _array[Length - 1] = default(T);
+                Length--;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Insert(T input, int index)
+        {
+            if (Length + 1 >= Capacity)
+            {
+                Array.Resize(ref _array, Capacity * 2);
+            }
+            if (index >= 0 && index < Length)
+            {
+                Array.Copy(_array, index, _array, index + 1, (Length - index));
+                _array[index] = input;
+                Length++;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("The index is outside the bounds of the array.");
+            }
+        }
+        public T this[int index]
+        {
+            get
+            {
+                if (index < Length)
+                {
+                    return _array[index];
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("The index is outside the bounds of the array.");
+                }
+            }
+            set
+            {
+                if (index < Length)
+                {
+                    _array[index] = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("The index is outside the bounds of the array.");
+                }
+
+            }
+        }
+    }
+}
---
+++ Ezhov_Ilya_Task08/Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 632;
+            int c = -12;
+            int[] arr = new int[] { 4, 8, 15, 16, 23, 42 };
+            Console.WriteLine("This program contains dynamic arrays class. Here is an examples of using this class.\n");
+            DynamicArray<int> dyn1 = new DynamicArray<int>();
+            Console.WriteLine($"1. Empty array of type int has been created. Capacity of array: {dyn1.Capacity}:");
+            Console.WriteLine($"2. Using method .Add to add our first element: {a}:");
+            dyn1.Add(a);
+            dyn1.ShowArray();
+            Console.WriteLine($"3. Using method .Add to add another element: {b}:");
+            dyn1.Add(b);
+            dyn1.ShowArray();
+            Console.WriteLine("4. Using method .AddRange to add array: { 4, 8, 15, 16, 23, 42 }:");
+            dyn1.AddRange(arr);
+            dyn1.ShowArray();
+            Console.WriteLine($"5. Using method .Remove to remove second element ({b}):");
+            dyn1.Remove(1);
+            dyn1.ShowArray();
+            Console.WriteLine($"6. Using method .Insert to insert element {b} at index {a}:");
+            dyn1.Insert(b, a);
+            dyn1.ShowArray();
+            Console.WriteLine($"7. Using [index] to set value {c} for the last element:");
+            dyn1[7] = c;
+            dyn1.ShowArray();
+
+            Console.WriteLine("End of program. Press any key.");
+            Console.WriteLine();
+        }
+    }
+}
