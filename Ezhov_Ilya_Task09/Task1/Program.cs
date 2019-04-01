6218fff8a02918aea950b3eaa0ce7cbbc5ed4c5e
Ilya Ezhov <angryhat@mail.ru>
2019-04-01 06:53:07 +0300

    Task 9 done

---
+++ Ezhov_Ilya_Task09/Task1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Person
+    {
+        public int PersonalNumber;
+
+        public Person(int number)
+        {
+            PersonalNumber = number;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = 158;
+            Console.WriteLine($"This program removes every second element from list and linked list. Number of elements in collections is {numberOfPeople}.");
+
+            List<Person> peopleList = new List<Person>();
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                peopleList.Add(new Person(i));
+            }
+            RemoveEveryRemoveEachSecondItem(peopleList);
+            foreach (var person in peopleList)
+            {
+                // to be sure there is only person left
+                Console.WriteLine($"\nThe lone survivor from List is person with number {person.PersonalNumber}.");
+            }
+
+            LinkedList<Person> peopleLinkedList = new LinkedList<Person>();
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                peopleLinkedList.AddLast(new Person(i));
+            }
+            RemoveEveryRemoveEachSecondItem(peopleLinkedList);
+            foreach (var person in peopleLinkedList)
+            {
+                // to be sure there is only person left
+                Console.WriteLine($"\nThe lone survivor from LinkedList is person with number {person.PersonalNumber}.");
+            }
+
+
+            Console.WriteLine("\n\nEnd of program. Press any key.");
+            Console.ReadKey();
+        }
+
+        public static void RemoveEveryRemoveEachSecondItem(List<Person> list)
+        {
+            while (list.Count != 1)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        list.RemoveAt(i);
+                    }
+                }
+            }
+        }
+        public static void RemoveEveryRemoveEachSecondItem(LinkedList<Person> list)
+        {
+            var current = list.First;
+            var next = current.Next;
+
+            while (list.Count != 1)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+
+                    if (i % 2 == 0 && current != null)
+                    {
+                        list.Remove(current);
+                    }
+                    current = next;
+                    if (next == null)
+                    {
+                        next = list.First;
+                    }
+                    else {
+                        next = next.Next;
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ Ezhov_Ilya_Task09/Task3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "there are wild and domestic animals. wild animals live in the forest or in the zoo." +
+                " some animals are dangerous. a fox, a wolf, a bear, a hare, a tiger, an elephant are wild animals." +
+                " domestic animals live at home or at the farm. they are dogs, cats, rabbits, cows, horses, parrots." +
+                " animals that we have at home are called pets.";
+
+            Dictionary<string, int> englishWords = new Dictionary<string, int>();
+
+            char[] separators = new char[] {' ', '.', ',', '!', '?'};
+            string[] textArray = text.Split(separators);
+
+            foreach (string word in textArray)
+            {
+                string wordIgnoreCase = word.ToLower();
+                if (englishWords.ContainsKey(wordIgnoreCase))
+                {
+                    englishWords[wordIgnoreCase] += 1;
+                }
+                else
+                {
+                    englishWords.Add(wordIgnoreCase, 1);
+                }
+            }
+
+            foreach (KeyValuePair<string, int> keyValue in englishWords)
+            {
+                Console.WriteLine(keyValue.Key + " - " + keyValue.Value);
+            }
+        }
+    }
+}
