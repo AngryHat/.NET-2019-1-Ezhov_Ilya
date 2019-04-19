902baff3107e3b43da5ba28c713daa344d8e3a0b
Ilya Ezhov <angryhat@mail.ru>
2019-04-19 14:55:55 +0300

    U&A Started once again

---
+++ Ezhov_Ilya_Task14/UsersAndAwards/Award.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersAndAwards
+{
+    class Award
+    {
+        public int id { get; set; }
+
+        private string _title { get; set; }
+        public string Title
+        {
+            get
+            {
+                return _title ?? "Unknown title";
+            }
+            set
+            {
+                if (value.Length < 50)
+                {
+                    _title = value;
+                }
+                //EXC
+            }
+        }
+
+        private string _decription { get; set; }
+        public string Decsrition
+        {
+            get
+            {
+                return _decription ?? "No description";
+            }
+            set
+            {
+                if (value.Length < 250)
+                {
+                    _decription = value;
+                }
+                //EXC
+            }
+        }
+
+    }
+}
