9c47b846aa5385e1c664898ecaec5a52f88a8dd6
Ilya Ezhov <angryhat@mail.ru>
2019-04-28 19:11:32 +0300

    Task 13 in progress

---
+++ Ezhov_Ilya_Task13/Task1/CurrentBankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class CurrentBankAccount : BankAccount
+    {
+        public CurrentBankAccount(string accountOwnerName, string accountNumber)
+            : base(accountOwnerName, accountNumber)
+        {
+            this.MinAccountBalance = 0m;
+            this.MaxDepositAmount = 100000000m;
+            InteresetRate = .25m;
+        }
+
+        public override void Deposit(decimal amount)
+        {
+            AccountBalance = AccountBalance + amount;
+            TransactionSummary = string.Format("{0}\n Deposit:{1}", TransactionSummary, amount);
+        }
+
+        public override void Withdraw(decimal amount)
+        {
+            if (AccountBalance - amount <= MinAccountBalance)
+            {
+                throw new Exception("You can not withdraw amount from your Current Account as Minimum Balance limit is reached");
+            }
+
+            AccountBalance = AccountBalance - amount;
+            TransactionSummary = string.Format("{0}\n Withdraw:{1}", TransactionSummary, amount);
+        }
+
+        public override void GenerateAccountReport()
+        {
+            Console.WriteLine("Current Account Report");
+            base.GenerateAccountReport();
+        }
+    }
+}
---
+++ Ezhov_Ilya_Task13/User-Defined Exceptions/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Defined_Exceptions
+{
+    public class MaxDepositAmountException : Exception
+    {
+        public MaxDepositAmountException()
+        {
+        }
+
+        public MaxDepositAmountException(string message) : base(message)
+        {
+        }
+
+        public MaxDepositAmountException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+
+    public class WithdrawCountException : Exception
+    {
+        public WithdrawCountException()
+        {
+        }
+
+        public WithdrawCountException(string message) : base(message)
+        {
+        }
+
+        public WithdrawCountException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+
+    public class OutOfBalanceException : Exception
+    {
+        public OutOfBalanceException()
+        {
+        }
+
+        public OutOfBalanceException(string message) : base(message)
+        {
+        }
+
+        public OutOfBalanceException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+
+    public class MinBalanceReachedException : Exception
+    {
+        public MinBalanceReachedException()
+        {
+        }
+
+        public MinBalanceReachedException(string message) : base(message)
+        {
+        }
+
+        public MinBalanceReachedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+
+    public class ReportGeneratingException : Exception
+    {
+        public ReportGeneratingException()
+        {
+        }
+
+        public ReportGeneratingException(string message) : base(message)
+        {
+        }
+
+        public ReportGeneratingException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
