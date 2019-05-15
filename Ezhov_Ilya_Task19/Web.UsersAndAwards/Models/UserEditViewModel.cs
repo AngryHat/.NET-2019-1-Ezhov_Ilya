61289d04d6b6fa5de61b387a83306a923266e6ce
Ilya Ezhov <angryhat@mail.ru>
2019-05-15 19:14:37 +0300

    Task 19 corrections[4]

--- Ezhov_Ilya_Task19/Web.UsersAndAwards/Models/UserEditViewModel.cs
+++ Ezhov_Ilya_Task19/Web.UsersAndAwards/Models/UserEditViewModel.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using BLL.Logic;
 using Entities;
 
 namespace Web.UsersAndAwards.Models
 {
-    public class UserWebViewModel
+    public class UserEditViewModel
     {
         public int id { get; set; }
         public string FirstName { get; set; }
@@ -14,16 +15,18 @@ namespace Web.UsersAndAwards.Models
         public string BirthDate { get; set; }
         public int Age { get; set; }
         public string AwardsToString { get; set; }
-        public List<Award> Awards { get; set; }
+        public List<Award> AllAwards { get; set; }
+        public List<AwardForCheckbox> Awards { get; set; }
 
-        public UserWebViewModel(User user)
+        public UserEditViewModel(User user, List<Award> allAwards)
         {
             id = user.id;
             FirstName = user.FirstName;
             LastName = user.LastName;
             Age = user.Age;
             BirthDate = user.BirthDate.ToShortDateString();
-            Awards = user.Awards ?? new List<Award>();
+            Awards = user.Awards.Select(a => new AwardForCheckbox(a)).ToList() ?? new List<AwardForCheckbox>();
+            AllAwards = allAwards;
             if (user.Awards.Count == 0)
             {
                 AwardsToString = "No awards, sorry";
@@ -32,6 +35,22 @@ namespace Web.UsersAndAwards.Models
             {
                 AwardsToString = string.Join(", ", Awards.Select(award => award.Title));
             }
+        }
+    }
+
+    public class AwardForCheckbox
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        bool IsChecked;
+
+        public AwardForCheckbox(Award award)
+        {
+            id = award.id;
+            Title = award.Title;
+            Description = award.Description;
+            IsChecked = true;
         }
     }
 }
